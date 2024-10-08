a6133259dc13e53e233b524dd6a9cb4108da058c
ykaraha5 <40692347@live.napier.ac.uk>
2024-10-08 21:29:18 +0100

    add and implement

---
+++ CityContoller.java
@@ -0,0 +1,13 @@
+public class CityContoller {
+    public class CityController {
+        private CityController populationService = new CityController();
+
+        public void displayCityPopulation() {
+            populationService.getCityController();
+            // Logic to display city population
+        }
+
+        private void getCityController() {
+        }
+
+        
---
+++ populationService.java
@@ -0,0 +1,11 @@
+public class populationService {
+    // Add methods to retrieve population data
+    public void getCountryPopulation() {
+        // Logic to retrieve data
+    }
+
+    public void getCityPopulation() {
+        // Logic to retrieve data
+    }
+}
+
